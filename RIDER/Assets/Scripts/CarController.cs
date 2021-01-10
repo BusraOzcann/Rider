a1d424dc95c19464b6d775240cd1fd3152a9fb02
Busra Ozcan <ozcan.busra1999@gmail.com>
2021-01-10 16:36:39 +0300

    ...

---
+++ RIDER/Assets/Scripts/CarController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed = 20f;
+    public float rotationSpeed;
+
+    bool move = false;
+    bool isGrounded = false;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            move = true;
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            move = false;
+        }
+
+        if (gameObject.transform.position.y < -11f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // update for inputs and fixedUpdate for physics controls
+        if (move == true)
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(transform.right * speed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+            }
+            else
+            {
+                // rotational force
+                rb.AddTorque(rotationSpeed * rotationSpeed * Time.fixedDeltaTime * 100f, ForceMode2D.Force);
+            }
+            
+        }
+    }
+
+    private void OnCollisionEnter2D()
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D()
+    {
+        isGrounded = false;
+    }
+}
